39313d54b0eaa1827a886820174fc05eebd4aa82
Okaka254 <fitzedgar9@gmail.com>
2022-09-15 19:15:08 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,9 @@
 
 void positive_or_negative(int i);
 
+int largest number(int a, int b, int c);
+void print remaining days(int month, int day, int year);
+int convert day(inth month, int day);
+
 #endif /* MAIN_H */
 
